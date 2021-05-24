723a0242ed0a71948419a4c69f73e8348eb89a1a
elena2020201 <elenarus1992@mail.ru>
2021-05-24 19:57:55 +0300

    Добавьте файлы проекта.

---
+++ симв массив_обратный порядок/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace симв_массив_обратный_порядок
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] symbs = new char[] { 'A', 'Z', 'B', 'Y', 'D', 'S', 'O' };
+            Console.WriteLine("Массив symbs:");
+            for(int k = 0; k < symbs.Length; k++)
+            {
+                Console.Write("| "+symbs[k] + " ");
+            }
+            Console.WriteLine("|");
+
+        Console.WriteLine("Массив symbs в обратном порядке:");
+            for (int k = symbs.Length-1;k>=0; k--)
+            {
+                Console.Write("| "+symbs[k] + " ");
+            }
+            Console.WriteLine("|");
+            Console.ReadLine();
+        }
+    }
+}
